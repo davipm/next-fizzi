72c19b7bd7efadc996eefaeb958603a82db150f4
Davi Pereira <davi.p.m94@gmail.com>
2025-06-21 17:11:07 +0900

    removing true from default prop

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -111,7 +111,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       {isDesktop && (
         <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
           <Scene />
-          <Bubbles count={300} speed={2} repeat={true} />
+          <Bubbles count={300} speed={2} repeat />
         </View>
       )}
 
