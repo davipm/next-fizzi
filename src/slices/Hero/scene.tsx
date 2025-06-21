e8211ce6edc8cb174b47c4568358ea95ab2c8af8
Davi Pereira <davi.p.m94@gmail.com>
2025-06-21 13:43:48 +0900

    Revert "Extract Hero animations into separate module and refactor animation logic"
    This reverts commit 010ab000749850c2f004a26cab1d5f8552837953.

--- src/slices/Hero/scene.tsx
+++ src/slices/Hero/scene.tsx
@@ -8,7 +8,6 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useStore } from "@/hooks/use-store";
 import FloatingCan from "@/components/floating-can";
-import { sceneAnimation } from "@/slices/Hero/hero.animation";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -29,18 +28,85 @@ export default function Scene() {
   const FLOAT_SPEED = 1.5;
 
   useGSAP(() => {
-    sceneAnimation({
-      can1Ref,
-      can2Ref,
-      can3Ref,
-      can4Ref,
-      can5Ref,
-      can1GroupRef,
-      can2GroupRef,
-      groupRef,
-    });
+    if (
+      !can1Ref.current ||
+      !can2Ref.current ||
+      !can3Ref.current ||
+      !can4Ref.current ||
+      !can5Ref.current ||
+      !can1GroupRef.current ||
+      !can2GroupRef.current ||
+      !groupRef.current
+    )
+      return;
 
     isReady();
+
+    gsap.set(can1Ref.current.position, { x: -1.5 });
+    gsap.set(can1Ref.current.rotation, { z: -0.5 });
+
+    gsap.set(can2Ref.current.position, { x: 1.5 });
+    gsap.set(can2Ref.current.rotation, { z: 0.5 });
+
+    gsap.set(can3Ref.current.position, { y: 5, z: 2 });
+    gsap.set(can4Ref.current.position, { x: 2, y: 4, z: 2 });
+    gsap.set(can5Ref.current.position, { y: -5 });
+
+    const introTl = gsap.timeline({
+      defaults: {
+        duration: 3,
+        easing: "back.out(1.4)",
+      },
+    });
+
+    if (window.scrollY < 20) {
+      introTl
+        .from(can1GroupRef.current.position, { y: -5, x: 1 }, 0)
+        .from(can1GroupRef.current.rotation, { z: 3 }, 0)
+        .from(can2GroupRef.current.position, { y: 5, x: 1 }, 0)
+        .from(can2GroupRef.current.rotation, { z: 3 }, 0);
+    }
+
+    const scrollTl = gsap.timeline({
+      defaults: {
+        duration: 2,
+      },
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+      },
+    });
+
+    scrollTl
+      // Rotate can group
+      .to(groupRef.current.rotation, { y: Math.PI * 2 })
+
+      // Can 1 - black cherry
+      .to(can1Ref.current.position, { x: -0.2, y: -0.7, z: -2 }, 0)
+      .to(can1Ref.current.rotation, { z: 0.3 }, 0)
+
+      // Can 2 - Lemon Lime
+      .to(can2Ref.current.position, { x: 1, y: -0.2, z: -1 }, 0)
+      .to(can2Ref.current.rotation, { z: 0 }, 0)
+
+      // Can 3 - Grape
+      .to(can3Ref.current.position, { x: -0.3, y: 0.5, z: -1 }, 0)
+      .to(can3Ref.current.rotation, { z: -0.1 }, 0)
+
+      // Can 4 - Strawberry Lemonade
+      .to(can4Ref.current.position, { x: 0, y: -0.3, z: 0.5 }, 0)
+      .to(can4Ref.current.rotation, { z: 0.3 }, 0)
+
+      // Can 5 -Watermelon
+      .to(can5Ref.current.position, { x: 0.3, y: 0.5, z: -0.5 }, 0)
+      .to(can5Ref.current.rotation, { z: -0.25 }, 0)
+      .to(
+        groupRef.current.position,
+        { x: 1, duration: 3, ease: "sine.inOut" },
+        1.3,
+      );
   });
 
   return (
