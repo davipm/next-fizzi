4209e5b2f623515371358f399a68cf6122c715a3
Davi Pereira <davi.p.m94@gmail.com>
2025-06-23 14:16:40 +0900

    adding clouds

--- src/slices/SkyDive/scene.tsx
+++ src/slices/SkyDive/scene.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import gsap from "gsap";
 import * as THREE from "three";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRef } from "react";
 import { Content } from "@prismicio/client";
 import { useGSAP } from "@gsap/react";
-import { Cloud, Clouds, Environment, Text } from "@react-three/drei";
 
+import { Cloud, Clouds, Environment, Text } from "@react-three/drei";
 import useMediaQuery from "@/hooks/use-media-query";
 import FloatingCan from "@/components/floating-can";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 /**
  * Renders a sentence as individual 3D text words using the @react-three/drei Text component.
@@ -101,11 +105,82 @@ export default function Scene({ sentence, flavor }: Props) {
     );
 
     // Spinning can
-    // TODO: Spinning can
+    gsap.to(canRef.current.position, {
+      y: Math.PI * 2,
+      duration: 1.7,
+      repeat: -1,
+      ease: "none",
+    });
+
+    const DISTANCE = 15;
+    const DURATION = 6;
+
+    gsap.set([cloud2Ref.current.position, cloud1Ref.current.position], {
+      ...getXYPositions(DISTANCE),
+    });
+
+    gsap.to(cloud1Ref.current.position, {
+      y: `+=${getYPosition(DISTANCE * 2)}`,
+      x: `+=${getXPosition(DISTANCE * -2)}`,
+      ease: "none",
+      repeat: -1,
+      duration: DURATION,
+    });
+
+    gsap.to(cloud2Ref.current.position, {
+      y: `+=${getYPosition(DISTANCE * 2)}`,
+      x: `+=${getXPosition(DISTANCE * -2)}`,
+      ease: "none",
+      repeat: -1,
+      delay: DURATION / 2,
+      duration: DURATION,
+    });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".skydive",
+        pin: true,
+        start: "top top",
+        end: "+=2000",
+        scrub: 1.5,
+      },
+    });
+
+    scrollTl
+      .to("body", {
+        backgroundColor: "#C0F0F5",
+        overflow: "auto",
+        duration: 0.1,
+      })
+      .to(cloudsRef.current.position, { z: 0, duration: 0.3 }, 0)
+      .to(canRef.current.position, {
+        x: 0,
+        y: 0,
+        duration: 0.3,
+        ease: "back.out(1.7)",
+      })
+      .to(
+        wordsRef.current.children.map((word) => word.position),
+        {
+          keyframes: [
+            { x: 0, y: 0, z: -1 },
+            { ...getXYPositions(-7), z: -7 },
+          ],
+          stagger: 0.3,
+        },
+        0,
+      )
+      .to(canRef.current.position, {
+        ...getXYPositions(4),
+        duration: 0.5,
+        ease: "back.in(1.7)",
+      })
+      .to(cloudsRef.current.position, { z: 7, duration: 0.5 });
   });
 
   return (
     <group ref={groupRef}>
+      {/* Can */}
       <group rotation={[0, 0, 0.5]}>
         <FloatingCan
           ref={canRef}
@@ -117,6 +192,17 @@ export default function Scene({ sentence, flavor }: Props) {
         <pointLight intensity={30} color="#8C0413" decay={0.6} />
       </group>
 
+      {/* Clouds */}
+      <Clouds ref={cloudsRef}>
+        <Cloud ref={cloud1Ref} bounds={[10, 10, 2]} />
+        <Cloud ref={cloud2Ref} bounds={[10, 10, 2]} />
+      </Clouds>
+
+      <group ref={wordsRef}>
+        {sentence && <ThreeText sentence={sentence} color="#F97315" />}
+      </group>
+
+      {/* Lights */}
       <ambientLight intensity={2} color="#9DDEFA" />
       <Environment files="/hdr/field.hdr" environmentIntensity={1.5} />
     </group>
